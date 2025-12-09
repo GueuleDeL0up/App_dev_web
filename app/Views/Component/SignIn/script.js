af9fe8126dff0d3b99f379d71016ef1d15477325
GueuleDeLoup <nico21.clement@gmail.com>
2025-12-09 23:51:23 +0100

    Reorganize files

--- app/Views/Component/SignIn/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("signInButton").addEventListener("click", function() {
-// Remonter deux dossiers pour atteindre app/Views/inscription.php
-window.location.href = "../../inscription.php";
-});
