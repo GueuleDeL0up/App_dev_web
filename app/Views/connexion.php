fbcce3e5ac2a3ef34f79b8f07938a5b4f2e04f5c
Vvv27v <vithues@hotmail.fr>
2025-12-12 22:25:35 +0100

    adaptation pour page responsive

--- app/Views/connexion.php
+++ app/Views/connexion.php
@@ -13,6 +13,7 @@ $base_url = "http://localhost/App_dev_web";
 
 <body>
 
+
     <!-- Partie logo -->
     <div class="logo-container">
         <img src="../../public/assets/images/KeepMyPet_Logo.png" alt="Logo KeepMyPet" class="logo">
@@ -24,31 +25,31 @@ $base_url = "http://localhost/App_dev_web";
 
 
     <!-- Formes du fond -->
+    <div class="shapes-container">
+        <!-- Ronds -->
+        <div class="shape circle" id="c1"></div>
+        <div class="shape circle" id="c2"></div>
+        <div class="shape circle" id="c3"></div>
+        <div class="shape circle" id="c4"></div>
+        <div class="shape circle" id="c5"></div>
+        <div class="shape circle" id="c6"></div>
+        <div class="shape circle" id="c7"></div>
+        <div class="shape circle" id="c8"></div>
+        <div class="shape circle" id="c9"></div>
+        <div class="shape circle" id="c10"></div>
 
-    <!-- Ronds -->
-    <div class="shape circle" id="c1"></div>
-    <div class="shape circle" id="c2"></div>
-    <div class="shape circle" id="c3"></div>
-    <div class="shape circle" id="c4"></div>
-    <div class="shape circle" id="c5"></div>
-    <div class="shape circle" id="c6"></div>
-    <div class="shape circle" id="c7"></div>
-    <div class="shape circle" id="c8"></div>
-    <div class="shape circle" id="c9"></div>
-    <div class="shape circle" id="c10"></div>
-
-
-    <!-- Triangles -->
-    <div class="shape triangle" id="t1"></div>
-    <div class="shape triangle" id="t2"></div>
-    <div class="shape triangle" id="t3"></div>
-    <div class="shape triangle" id="t4"></div>
-    <div class="shape triangle" id="t5"></div>
-    <div class="shape triangle" id="t6"></div>
-    <div class="shape triangle" id="t7"></div>
-    <div class="shape triangle" id="t8"></div>
-    <div class="shape triangle" id="t9"></div>
-    <div class="shape triangle" id="t10"></div>
+        <!-- Triangles -->
+        <div class="shape triangle" id="t1"></div>
+        <div class="shape triangle" id="t2"></div>
+        <div class="shape triangle" id="t3"></div>
+        <div class="shape triangle" id="t4"></div>
+        <div class="shape triangle" id="t5"></div>
+        <div class="shape triangle" id="t6"></div>
+        <div class="shape triangle" id="t7"></div>
+        <div class="shape triangle" id="t8"></div>
+        <div class="shape triangle" id="t9"></div>
+        <div class="shape triangle" id="t10"></div>
+    </div>
 
 </body>
 
