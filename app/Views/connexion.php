18834e84d2ec28c3b7ad9e36c269c28ea9cb98f8
Vvv27v <vithues@hotmail.fr>
2025-12-12 23:03:53 +0100

    Copie du contenu de log-in dans sign-up

--- app/Views/connexion.php
+++ app/Views/connexion.php
@@ -1,49 +1,56 @@
-<?php require_once '/app/Views/header.php'; ?>
+<?php
+$base_url = "http://localhost/App_dev_web";
+?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-<meta charset="UTF-8">
-<title>KeepMyPet - Login</title>
-<link rel="stylesheet" href="assets/css/main.css">
+    <meta charset="UTF-8">
+    <title>KeepMyPet - Login</title>
+    <!-- link rel="stylesheet" href="../../public/assets/css/main.css"-->
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/public/assets/css/connexion.css">
 </head>
-<body id="connexion">
 
-<!-- Formes du fond -->
-<div class="shape circle" style="top: 120px; left: 150px;"></div>
-<div class="shape circle" style="top: 300px; left: 100px;"></div>
-<div class="shape circle" style="bottom: 120px; left: 200px;"></div>
-<div class="shape triangle" style="top: 250px; left: 350px;"></div>
-<div class="shape triangle" style="bottom: 200px; left: 100px;"></div>
-<div class="shape circle" style="top: 80px; right: 350px;"></div>
-<div class="shape triangle" style="top: 200px; right: 220px;"></div>
-<div class="shape circle" style="bottom: 140px; right: 300px;"></div>
+<body>
 
-<div class="container">
 
     <!-- Partie logo -->
-    <div class="left">
-        <img src="assets/KeepMyPet_Logo.png" alt="Logo KeepMyPet">
+    <div class="logo-container">
+        <img src="../../public/assets/images/KeepMyPet_Logo.png" alt="Logo KeepMyPet" class="logo">
     </div>
 
-    <!-- Partie formulaire -->
-    <div class="right">
-        <label>IDENTIFIANT</label>
-        <input type="text" class="input">
-
-        <label>MOT DE PASSE</label>
-        <input type="password" class="input">
-
-        
-        <a href="Accueil_utilisateur.html"><button class="btn">Se connecter</button></a>
-
-
-        <div class="links">
-            <a href="#">Mot de passe oublié</a> – 
-            <a href="Inscription.HTML">S’inscrire</a>
-        </div>
+    <div class="login-container">
+        <?php include 'Components/LogIn/log_in.php'; ?>
     </div>
-</div>
+
+
+    <!-- Formes du fond -->
+    <div class="shapes-container">
+        <!-- Ronds -->
+        <div class="shape circle" id="c1"></div>
+        <div class="shape circle" id="c2"></div>
+        <div class="shape circle" id="c3"></div>
+        <div class="shape circle" id="c4"></div>
+        <div class="shape circle" id="c5"></div>
+        <div class="shape circle" id="c6"></div>
+        <div class="shape circle" id="c7"></div>
+        <div class="shape circle" id="c8"></div>
+        <div class="shape circle" id="c9"></div>
+        <div class="shape circle" id="c10"></div>
+
+        <!-- Triangles -->
+        <div class="shape triangle" id="t1"></div>
+        <div class="shape triangle" id="t2"></div>
+        <div class="shape triangle" id="t3"></div>
+        <div class="shape triangle" id="t4"></div>
+        <div class="shape triangle" id="t5"></div>
+        <div class="shape triangle" id="t6"></div>
+        <div class="shape triangle" id="t7"></div>
+        <div class="shape triangle" id="t8"></div>
+        <div class="shape triangle" id="t9"></div>
+        <div class="shape triangle" id="t10"></div>
+    </div>
 
 </body>
+
 </html>
-<?php require_once '/app/Views/footer.php'; ?>
