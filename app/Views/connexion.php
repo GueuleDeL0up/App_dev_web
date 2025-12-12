a93c5735c1e6cae3b62de007a248f4d3b1a84810
Vvv27v <vithues@hotmail.fr>
2025-12-12 15:42:09 +0100

    creation de doc css pour differencier code

--- app/Views/connexion.php
+++ app/Views/connexion.php
@@ -3,29 +3,50 @@
 <head>
 <meta charset="UTF-8">
 <title>KeepMyPet - Login</title>
-<link rel="stylesheet" href="../../public/assets/css/main.css">
+<!-- link rel="stylesheet" href="../../public/assets/css/main.css"-->
+<link rel="stylesheet" href="../../public/assets/css/connexion.css">
 </head>
 <body id="connexion">
 
 <!-- Formes du fond -->
-<div class="shape circle" style="top: 120px; left: 150px;"></div>
-<div class="shape circle" style="top: 300px; left: 100px;"></div>
-<div class="shape circle" style="bottom: 120px; left: 200px;"></div>
-<div class="shape triangle" style="top: 250px; left: 350px;"></div>
-<div class="shape triangle" style="bottom: 200px; left: 100px;"></div>
-<div class="shape circle" style="top: 80px; right: 350px;"></div>
-<div class="shape triangle" style="top: 200px; right: 220px;"></div>
-<div class="shape circle" style="bottom: 140px; right: 300px;"></div>
+
+<!-- Ronds -->
+<div class="shape circle" id="c1"></div>
+<div class="shape circle" id="c2"></div>
+<div class="shape circle" id="c3"></div>
+<div class="shape circle" id="c4"></div>
+<div class="shape circle" id="c5"></div>
+<div class="shape circle" id="c6"></div>
+<div class="shape circle" id="c7"></div>
+<div class="shape circle" id="c8"></div>
+<div class="shape circle" id="c9"></div>
+<div class="shape circle" id="c10"></div>
+
+
+<!-- Triangles -->
+<div class="shape triangle" id="t1"></div>
+<div class="shape triangle" id="t2"></div>
+<div class="shape triangle" id="t3"></div>
+<div class="shape triangle" id="t4"></div>
+<div class="shape triangle" id="t5"></div>
+<div class="shape triangle" id="t6"></div>
+<div class="shape triangle" id="t7"></div>
+<div class="shape triangle" id="t8"></div>
+<div class="shape triangle" id="t9"></div>
+<div class="shape triangle" id="t10"></div>
+
+
+
 
 <div class="container">
 
     <!-- Partie logo -->
     <div class="left">
-        <img src="../../public/assets/images/KeepMyPet_Logo.png" alt="Logo KeepMyPet">
+        <img src="../../public/assets/images/KeepMyPet_Logo.png" alt="Logo KeepMyPet" class="logo">
     </div>
 
     <!-- Partie formulaire -->
-    <div class="right">
+    <!--div class="right">
         <label>IDENTIFIANT</label>
         <input type="text" class="input">
 
@@ -40,7 +61,7 @@
             <a href="#">Mot de passe oublié</a> – 
             <a href="Inscription.HTML">S’inscrire</a>
         </div>
-    </div>
+    </div -->
 </div>
 
 </body>
