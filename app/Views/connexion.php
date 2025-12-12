dbc0642cf3864b606def4a95da8cacad8d92881f
Vvv27v <vithues@hotmail.fr>
2025-12-12 17:33:52 +0100

    alignement container, page connexion

--- app/Views/connexion.php
+++ app/Views/connexion.php
@@ -1,68 +1,55 @@
+<?php
+$base_url = "http://localhost/App_dev_web";
+?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-<meta charset="UTF-8">
-<title>KeepMyPet - Login</title>
-<!-- link rel="stylesheet" href="../../public/assets/css/main.css"-->
-<link rel="stylesheet" href="../../public/assets/css/connexion.css">
+    <meta charset="UTF-8">
+    <title>KeepMyPet - Login</title>
+    <!-- link rel="stylesheet" href="../../public/assets/css/main.css"-->
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/public/assets/css/connexion.css">
 </head>
-<body id="connexion">
 
-<!-- Formes du fond -->
-
-<!-- Ronds -->
-<div class="shape circle" id="c1"></div>
-<div class="shape circle" id="c2"></div>
-<div class="shape circle" id="c3"></div>
-<div class="shape circle" id="c4"></div>
-<div class="shape circle" id="c5"></div>
-<div class="shape circle" id="c6"></div>
-<div class="shape circle" id="c7"></div>
-<div class="shape circle" id="c8"></div>
-<div class="shape circle" id="c9"></div>
-<div class="shape circle" id="c10"></div>
-
-
-<!-- Triangles -->
-<div class="shape triangle" id="t1"></div>
-<div class="shape triangle" id="t2"></div>
-<div class="shape triangle" id="t3"></div>
-<div class="shape triangle" id="t4"></div>
-<div class="shape triangle" id="t5"></div>
-<div class="shape triangle" id="t6"></div>
-<div class="shape triangle" id="t7"></div>
-<div class="shape triangle" id="t8"></div>
-<div class="shape triangle" id="t9"></div>
-<div class="shape triangle" id="t10"></div>
-
-
-
-
-<div class="container">
+<body>
 
     <!-- Partie logo -->
-    <div class="left">
+    <div class="logo-container">
         <img src="../../public/assets/images/KeepMyPet_Logo.png" alt="Logo KeepMyPet" class="logo">
     </div>
 
-    <!-- Partie formulaire -->
-    <!--div class="right">
-        <label>IDENTIFIANT</label>
-        <input type="text" class="input">
-
-        <label>MOT DE PASSE</label>
-        <input type="password" class="input">
-
-        
-        <a href="Accueil_utilisateur.html"><button class="btn">Se connecter</button></a>
+    <div class="login-container">
+        <?php include 'Components/LogIn/log_in.php'; ?>
+    </div>
 
 
-        <div class="links">
-            <a href="#">Mot de passe oublié</a> – 
-            <a href="Inscription.HTML">S’inscrire</a>
-        </div>
-    </div -->
-</div>
+    <!-- Formes du fond -->
+
+    <!-- Ronds -->
+    <div class="shape circle" id="c1"></div>
+    <div class="shape circle" id="c2"></div>
+    <div class="shape circle" id="c3"></div>
+    <div class="shape circle" id="c4"></div>
+    <div class="shape circle" id="c5"></div>
+    <div class="shape circle" id="c6"></div>
+    <div class="shape circle" id="c7"></div>
+    <div class="shape circle" id="c8"></div>
+    <div class="shape circle" id="c9"></div>
+    <div class="shape circle" id="c10"></div>
+
+
+    <!-- Triangles -->
+    <div class="shape triangle" id="t1"></div>
+    <div class="shape triangle" id="t2"></div>
+    <div class="shape triangle" id="t3"></div>
+    <div class="shape triangle" id="t4"></div>
+    <div class="shape triangle" id="t5"></div>
+    <div class="shape triangle" id="t6"></div>
+    <div class="shape triangle" id="t7"></div>
+    <div class="shape triangle" id="t8"></div>
+    <div class="shape triangle" id="t9"></div>
+    <div class="shape triangle" id="t10"></div>
 
 </body>
+
 </html>
