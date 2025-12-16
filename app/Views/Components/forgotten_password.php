a891f27688718a77ff29e95a43d209100c0d5007
Bryan De Faria <bryan.de-faria@eleve.isep.fr>
2025-12-16 21:16:27 +0100

    minor changes on syntaxe

--- app/Views/Components/forgotten_password.php
+++ app/Views/Components/forgotten_password.php
@@ -2,15 +2,15 @@
 // Define the base
 $base_url = "/App_dev_web/";  // For HTML links
 $base_dir = __DIR__ . "/../../../";  // For PHP includes
-
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Mot de passe oublié</title>
-  <link rel="stylesheet" href= "../../../public/assets/css/forgotten_password.css">
+  <link rel="stylesheet" href= "<?php echo $base_url; ?>/public/assets/css/forgotten_password.css">
 </head>
 
 <body>
