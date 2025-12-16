6447060c57544212e8c863f80887c82e1b0dcb7c
Vvv27v <vithues@hotmail.fr>
2025-12-16 17:02:13 +0100

    create page forgotten_password

--- app/Views/Components/LogIn/log_in.php
+++ app/Views/Components/LogIn/log_in.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +10,7 @@
 </head>
 
 <body>
+  <?php include 'Components/LogIn/log_in.php'; ?>
 
   <div class="right">
     <label>IDENTIFIANT</label>
